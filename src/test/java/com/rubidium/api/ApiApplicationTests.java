c4a17cd88f3fa205ad5985d7c9b2eac451f0dcef
lrod <ballestax01@gmail.com>
2025-06-27 16:24:09 -0500

    first commit

---
+++ src/test/java/com/rubidium/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubidium.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
